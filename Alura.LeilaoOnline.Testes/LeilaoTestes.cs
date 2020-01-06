ee891722e35717aa5b0a83fc4d26df0151f4c609
Gustavo <gustavobuenodemoraes@yahoo.com.br>
2020-01-05 22:19:30 -0300

    O que devo testar

--- Alura.LeilaoOnline.Testes/LeilaoTestes.cs
+++ Alura.LeilaoOnline.Testes/LeilaoTestes.cs
@@ -6,6 +6,57 @@ namespace Alura.LeilaoOnline.Testes
     public class LeilaoTestes
     {
         [Fact]
+        public void LeilaoComTresClientes()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+            var beltrano = new Interessada("Beltrano", leilao);
+
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 990);
+            leilao.RecebeLance(beltrano, 1400);
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 1400;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+            Assert.Equal(beltrano, leilao.Ganhador.Cliente);
+        }
+
+        [Fact]
+        public void LeilaoComLancesOrdenadosPorValor()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(maria, 990);
+            leilao.RecebeLance(fulano, 1000);
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
         public void LeilaoComUnicoValores()
         {
             //Arranje - cenário
@@ -35,12 +86,13 @@ namespace Alura.LeilaoOnline.Testes
             leilao.RecebeLance(fulano, 800);
             leilao.RecebeLance(maria, 900);
             leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(fulano, 1100);
 
             //Act - método sob teste
             leilao.TerminaPregao();
 
             //Assert
-            var valorEsperado = 1000;
+            var valorEsperado = 1100;
             var valorObtido = leilao.Ganhador.Valor;
 
             Assert.Equal(valorEsperado, valorObtido);
