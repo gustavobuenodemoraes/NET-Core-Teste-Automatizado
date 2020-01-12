0bfc24592ced2fe36d444708363f0dace37303a7
Gustavo <gustavobuenodemoraes@yahoo.com.br>
2020-01-12 14:59:48 -0300

    Testando o registro

---
+++ ALura.LeilaoOnline.Selenium/Fixtures/TestFixture.cs
@@ -0,0 +1,24 @@
+﻿using ALura.LeilaoOnline.Selenium.Helpers;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ALura.LeilaoOnline.Selenium.Fixtures
+{
+    public class TestFixture : IDisposable
+    {
+        public IWebDriver Driver { get; set; }
+        //Setup
+        public TestFixture()
+        {
+            Driver = new ChromeDriver(TestHelper.PastaDoExecutavel);
+        }
+
+        public void Dispose()
+        {
+            Driver.Quit();
+        }
+    }
+}
---
+++ ALura.LeilaoOnline.Selenium/Helpers/TestHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace ALura.LeilaoOnline.Selenium.Helpers
+{
+    public static class TestHelper
+    {
+        public static string PastaDoExecutavel => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+    }
+}
