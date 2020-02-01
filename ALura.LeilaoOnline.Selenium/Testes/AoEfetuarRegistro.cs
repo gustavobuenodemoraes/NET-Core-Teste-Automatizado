6ff024b54fdc4e1a519168dafa573d7cd15f5073
Gustavo <gustavobuenodemoraes@yahoo.com.br>
2020-02-01 18:25:53 -0300

    Testando o registro

---
+++ ALura.LeilaoOnline.Selenium/Testes/AoEfetuarRegistro.cs
@@ -0,0 +1,25 @@
+﻿using ALura.LeilaoOnline.Selenium.Fixtures;
+using OpenQA.Selenium;
+using Xunit;
+namespace ALura.LeilaoOnline.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoEfetuarRegistro
+    {
+        private readonly IWebDriver _driver;
+
+        public AoEfetuarRegistro(TestFixture fixture)
+        {
+            _driver = fixture.Driver;
+        }
+
+        [Fact]
+        public void Teste()
+        {
+            //arrange
+            _driver.Navigate().GoToUrl("http://localhost:5000");
+            //act
+            //assert
+        }
+    }
+}
