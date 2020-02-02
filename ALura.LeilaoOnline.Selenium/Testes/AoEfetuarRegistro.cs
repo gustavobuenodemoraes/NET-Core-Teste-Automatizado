8fc7d1303f80d3b590e6b7d876e64c1e95b2c56c
Gustavo <gustavobuenodemoraes@yahoo.com.br>
2020-02-01 21:10:15 -0300

    Obtendo elementos

--- ALura.LeilaoOnline.Selenium/Testes/AoEfetuarRegistro.cs
+++ ALura.LeilaoOnline.Selenium/Testes/AoEfetuarRegistro.cs
@@ -14,12 +14,30 @@ namespace ALura.LeilaoOnline.Selenium.Testes
         }
 
         [Fact]
-        public void Teste()
+        public void DadosValidosDoFormularioENavegarParaProximaPagina()
         {
             //arrange
             _driver.Navigate().GoToUrl("http://localhost:5000");
+
+            //nome
+            var inputNome = _driver.FindElement(By.Id("Nome"));
+            //email
+            var inputEmail = _driver.FindElement(By.Id("Email"));
+            //password
+            var inputSenha = _driver.FindElement(By.Id("Password"));
+            //confirmpassword
+            var inputConfirmSenha = _driver.FindElement(By.Id("ConfirmPassword"));
+
+            var botaoRegistro = _driver.FindElement(By.Id("btnRegistro"));
+
+            inputNome.SendKeys("Gustavo Bueno");
+            inputEmail.SendKeys("gustavo@teste.com");
+            inputSenha.SendKeys("123456");
+            inputConfirmSenha.SendKeys("123456");
             //act
+            botaoRegistro.Click();
             //assert
+            Assert.Contains("Obrigado", _driver.PageSource);
         }
     }
 }
