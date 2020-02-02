b0b94fa747c7e9de53c761b35ec4f4cc05df19c1
Gustavo <gustavobuenodemoraes@yahoo.com.br>
2020-02-02 00:37:05 -0300

    Mensagens de erro

--- ALura.LeilaoOnline.Selenium/Testes/AoEfetuarRegistro.cs
+++ ALura.LeilaoOnline.Selenium/Testes/AoEfetuarRegistro.cs
@@ -39,5 +39,55 @@ namespace ALura.LeilaoOnline.Selenium.Testes
             //assert
             Assert.Contains("Obrigado", _driver.PageSource);
         }
+
+        [Theory]
+        [InlineData("", "gustavo@teste.com", "123456", "123456")]
+        [InlineData("Gustavo Bueno", "gustavo.com", "123456", "123456")]
+        [InlineData("Gustavo Bueno", "gustavo@teste.com", "123456", "444788")]
+        [InlineData("Gustavo Bueno", "gustavo@teste.com", "123456", "")]
+        public void DadosInvalidosDoFormularioENavegarParaProximaPagina(
+            string nome,
+            string email,
+            string senha,
+            string confirmSenha)
+        {
+            //arrange
+            _driver.Navigate().GoToUrl("http://localhost:5000");
+
+            //nome
+            var inputNome = _driver.FindElement(By.Id("Nome"));
+            //email
+            var inputEmail = _driver.FindElement(By.Id("Email"));
+            //password
+            var inputSenha = _driver.FindElement(By.Id("Password"));
+            //confirmpassword
+            var inputConfirmSenha = _driver.FindElement(By.Id("ConfirmPassword"));
+
+            var botaoRegistro = _driver.FindElement(By.Id("btnRegistro"));
+
+            inputNome.SendKeys(nome);
+            inputEmail.SendKeys(email);
+            inputSenha.SendKeys(senha);
+            inputConfirmSenha.SendKeys(confirmSenha);
+
+            //act
+            botaoRegistro.Click();
+            //assert
+            Assert.Contains("section-registro", _driver.PageSource);
+        }
+
+        [Fact]
+        public void DadoNomeEmBrancoDeveMostrarMensagemDeErro()
+        {
+            //arrange
+            _driver.Navigate().GoToUrl("http://localhost:5000");
+
+            var botaoRegistro = _driver.FindElement(By.Id("btnRegistro"));
+
+            //act
+            botaoRegistro.Click();
+            //assert
+            Assert.True("The Nome field is required.", _driver.PageSource);
+        }
     }
 }
